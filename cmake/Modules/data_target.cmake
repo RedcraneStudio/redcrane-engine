8cbad8beced8c2e42745e12e2d111797d206aa1a
Luke San Antonio <lukesanantonio@gmail.com>
2015-03-22 23:00:43 -0400

    Added the data_target cmake file I wrote for Tommy's game.

---
+++ cmake/Modules/data_target.cmake
@@ -0,0 +1,17 @@
+
+# Copyright (C) 2014 Luke San Antonio
+# All rights reserved.
+
+function(add_data target all_or_nah)
+  add_custom_target(${target} ${all_or_nah})
+
+  message(STATUS "${ARGN}")
+  foreach(filename ${ARGN})
+    add_custom_command(TARGET ${target} COMMAND ${CMAKE_COMMAND}
+                       ARGS -E copy_if_different
+                       ${CMAKE_CURRENT_SOURCE_DIR}/${filename}
+                       ${CMAKE_CURRENT_BINARY_DIR}/${filename}
+                       MAIN_DEPENDENCY ${filename})
+    install(FILES ${filename} DESTINATION share/${PROJECT_NAME})
+  endforeach(filename)
+endfunction()
