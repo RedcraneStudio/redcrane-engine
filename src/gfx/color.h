418ae44804f65097a99ee243adb3bc5ad0c9b297
Luke San Antonio <lukesanantonio@gmail.com>
2015-03-29 16:03:41 -0400

    Added simple color struct.

---
+++ src/gfx/color.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2014 Luke San Antonio
+ * All rights reserved.
+ */
+#include <cstdint>
+namespace survive
+{
+  struct Color
+  {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+  };
+}
