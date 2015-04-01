01da388b60f3a992501ffa2cfea00fa6bef19348
Luke San Antonio <lukesanantonio@gmail.com>
2015-04-01 14:52:06 -0400

    The gfx::Object is now somewhat ready. It has a loading function and a render function.
    This commit basically updates it to the new driver interface (no more
    ifactory).

--- src/gfx/object.cpp
+++ src/gfx/object.cpp
@@ -7,10 +7,18 @@ namespace survive
 {
   namespace gfx
   {
-    void Object::render() const noexcept
+    Object create_object(gfx::IDriver& driver, std::string obj,
+                         std::string mat) noexcept
     {
-      material->use();
-      mesh->render();
+      auto ret = Object{};
+      ret.mesh = driver.prepare_mesh(Mesh::from_file(obj));
+      ret.material = load_material(mat);
+      return ret;
+    }
+    void object_render(Object const& obj) noexcept
+    {
+      obj.material->use();
+      obj.mesh->render();
     }
   }
 }
--- src/gfx/object.h
+++ src/gfx/object.h
@@ -5,9 +5,9 @@
 #pragma once
 #include <memory>
 #include "prepared_mesh.h"
-#include "material.h"
+#include "imaterial.h"
 #include "../maybe_owned.hpp"
-#include "ifactory.h"
+#include "idriver.h"
 namespace survive
 {
   namespace gfx
@@ -16,17 +16,11 @@ namespace survive
     {
       Maybe_Owned<Prepared_Mesh> mesh;
       Maybe_Owned<IMaterial> material;
-
-      void render() const noexcept;
     };
 
-    Object create_object(gfx::IFactory& fact, std::string obj,
-                         std::string mat) noexcept
-    {
-      auto ret = Object{};
-      ret.mesh = fact.prepare_mesh(Mesh::from_file(obj));
-      ret.material = load_material(mat);
-      return ret;
-    }
+    Object create_object(gfx::IDriver& driver, std::string obj,
+                         std::string mat) noexcept;
+
+    void render_object(Object const&) noexcept;
   }
 }
