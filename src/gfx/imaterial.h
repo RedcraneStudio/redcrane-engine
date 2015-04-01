121d6b7320675b7da466f6be3982c6f7fc3d6342
Luke San Antonio <lukesanantonio@gmail.com>
2015-04-01 13:41:54 -0400

    A material is now a scene observer.

--- src/gfx/imaterial.h
+++ src/gfx/imaterial.h
@@ -3,14 +3,18 @@
  * All rights reserved.
  */
 #pragma once
+#include "iscene_observer.h"
 namespace survive
 {
   namespace gfx
   {
-    struct IMaterial
+    struct IMaterial : public IScene_Observer
     {
       virtual ~IMaterial() noexcept {}
       virtual void use() const noexcept = 0;
+
+      void set_projection(glm::mat4 const& proj) noexcept = 0;
+      void set_view(glm::mat4 const& view) noexcept = 0;
     };
   }
 }
