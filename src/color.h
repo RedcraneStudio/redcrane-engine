94ba37110ca68d0a7d65939fccc1b4dfe4d5bbf4
Luke San Antonio <lukesanantonio@gmail.com>
2015-04-01 16:32:02 -0400

    The Color structure learned equality (and inequality) operators.

--- src/color.h
+++ src/color.h
@@ -11,4 +11,13 @@ namespace survive
     uint8_t g;
     uint8_t b;
   };
+
+  inline bool operator==(Color const& c1, Color const& c2) noexcept
+  {
+    return c1.r == c2.r && c1.g == c2.g && c1.b == c2.b;
+  }
+  inline bool operator!=(Color const& c1, Color const& c2) noexcept
+  {
+    return !(c1 == c2);
+  }
 }
