bd1638d8a735751d596ebdbb2905852d75f493aa
Luke San Antonio <lukesanantonio@gmail.com>
2015-04-03 22:10:25 -0400

    Added a template for AABBs.

---
+++ src/common/aabb.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+namespace strat
+{
+  template <class T>
+  struct AABB
+  {
+    T depth;
+    T height;
+    T width;
+  };
+}
