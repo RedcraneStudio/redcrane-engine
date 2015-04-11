461dd4c2c549c76f1ae6fc14d70d9a50fd2f344e
Luke San Antonio <lukesanantonio@gmail.com>
2015-04-11 13:37:02 -0400

    Added a concrete POD structure to represent a camera.

---
+++ src/gfx/camera.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+#include <glm/glm.hpp>
+namespace strat
+{
+  namespace gfx
+  {
+    enum class Camera_Type
+    {
+      Perspective, Orthographic
+    };
+
+    struct Perspective_Cam_Params
+    {
+      float fov, aspect, width, height, near, far;
+    };
+
+    struct Ortho_Cam_Params
+    {
+      float left, right, bottom, top, near, far;
+    };
+
+    struct Camera
+    {
+      // A camera also includes a perspective and orthographic mode.
+      Camera_Type projection_mode;
+      union
+      {
+        Perspective_Cam_Params perspective;
+        Ortho_Cam_Params ortho;
+      };
+
+      // Camera position, orientation and lookpos.
+      glm::vec3 pos;
+      glm::vec3 look;
+      glm::vec3 up;
+    };
+  }
+}
