a9f70cbc753c10869d4cfd860b6e4d8047c6c211
Luke San Antonio <lukesanantonio@gmail.com>
2015-04-12 01:42:31 -0400

    Added the ol' animation class from Tommy's game.

---
+++ src/common/animation.cpp
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2014 Luke San Antonio
+ * All rights reserved.
+ */
+#include "animation.h"
+namespace strat
+{
+  // TODO: ***TEST THIS CLASS***
+  Animation::Animation(int segment_size, int segments,
+                       Anim_Repeat_Mode repeat,
+                       segment_fn_t segment_fn,
+                       substep_fn_t substep_fn) noexcept
+                       : segment_size_(segment_size + 1),
+                         segments_(segments),
+                         repeat_(repeat),
+                         segment_fn_(segment_fn),
+                         substep_fn_(substep_fn) {}
+
+  void Animation::step() noexcept
+  {
+    if(done()) return;
+
+    int segment = cur_ / segment_size_;
+    int substep = cur_ % segment_size_;
+
+    // When our substep is 0 it means we just started a new segment.
+    if(substep == 0)
+    {
+      if(segment_fn_) segment_fn_(segment);
+    }
+    // When it is 1 or above it means it is our first and subsequent steps into
+    // the segment respectively.
+    else if(substep != 0)
+    {
+      if(substep_fn_) substep_fn_(substep - 1);
+    }
+
+    // Do we have to go in reverse or start over or neither?
+    // Note the last case is covered by the done() conditional at the beginning
+    // of this function.
+    if(repeat_ == Anim_Repeat_Mode::Begin_Repeat && at_endpoint_())
+    {
+      // Repeat ourselves from the beginning.
+      cur_ = 0;
+    }
+    else if(repeat_ == Anim_Repeat_Mode::Reverse_Repeat && at_endpoint_())
+    {
+      going_reverse_ = !going_reverse_;
+      incremement_();
+    }
+    else
+    {
+      incremement_();
+    }
+  }
+  bool Animation::done() const noexcept
+  {
+    return cur_ >= segments_ * segment_size_ &&
+           repeat_ == Anim_Repeat_Mode::No_Repeat;
+  }
+
+  void Animation::reset() noexcept
+  {
+    cur_ = 0;
+    going_reverse_ = false;
+  }
+  void Animation::reset(int segment_size, int segments,
+                        Anim_Repeat_Mode repeat) noexcept
+  {
+    reset();
+
+    segment_size_ = segment_size;
+    segments_ = segments;
+    repeat_ = repeat;
+  }
+  bool Animation::at_endpoint_() const noexcept
+  {
+    // Endpoints are checked inclusively, while the animation cur_ loops to
+    // [0, segments_ * segment_size_).
+    if(going_reverse_) return cur_ == 0;
+    return cur_ == (segments_ * segment_size_) - 1;
+  }
+  void Animation::incremement_() noexcept
+  {
+    if(going_reverse_) --cur_;
+    else ++cur_;
+  }
+}
---
+++ src/common/animation.h
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+#include <functional>
+namespace strat
+{
+  enum Anim_Repeat_Mode
+  {
+    No_Repeat,
+    Begin_Repeat,
+    Reverse_Repeat
+  };
+
+  /*!
+   * If the segment size is 5 and the amount of segments is 2:
+   * **Start of segment 1**
+   * segment_fn(0)
+
+   * substep_fn(0)
+   * substep_fn(1)
+   * substep_fn(2)
+   * substep_fn(3)
+   * substep_fn(4)
+
+   * **Start of segment 2**
+   * segment_fn(1)
+
+   * substep_fn(0)
+   * substep_fn(1)
+   * substep_fn(2)
+   * substep_fn(3)
+   * substep_fn(4)
+   */
+  struct Animation
+  {
+    using segment_fn_t = std::function<void (int)>;
+    using substep_fn_t = segment_fn_t;
+
+    Animation(int segment_size, int segments,
+              Anim_Repeat_Mode repeat = Anim_Repeat_Mode::No_Repeat,
+              segment_fn_t segment_fn = nullptr,
+              substep_fn_t substep_fn = nullptr) noexcept;
+
+    inline void set_segment_fn(segment_fn_t segment_fn) noexcept;
+    inline void set_substep_fn(substep_fn_t substep_fn) noexcept;
+
+    inline int segment_size() const noexcept;
+    inline int segments() const noexcept;
+    inline Anim_Repeat_Mode repeat_mode() const noexcept;
+
+    void step() noexcept;
+
+    bool done() const noexcept;
+
+    void reset() noexcept;
+    void reset(int segment_size, int segments,
+               Anim_Repeat_Mode repeat) noexcept;
+
+  private:
+    int cur_ = 0;
+
+    bool going_reverse_ = false;
+
+    bool at_endpoint_() const noexcept;
+    void incremement_() noexcept;
+
+    int segment_size_;
+    int segments_;
+    Anim_Repeat_Mode repeat_;
+    segment_fn_t segment_fn_;
+    substep_fn_t substep_fn_;
+  };
+
+  inline void Animation::set_segment_fn(segment_fn_t segment_fn) noexcept
+  {
+    segment_fn_ = segment_fn;;
+  }
+  inline void Animation::set_substep_fn(substep_fn_t substep_fn) noexcept
+  {
+    substep_fn_ = substep_fn;
+  }
+
+  inline int Animation::segment_size() const noexcept
+  {
+    return segment_size_;
+  }
+  inline int Animation::segments() const noexcept
+  {
+    return segments_;
+  }
+  inline Anim_Repeat_Mode Animation::repeat_mode() const noexcept
+  {
+    return repeat_;
+  }
+}
