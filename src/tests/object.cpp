b234171a171b6496fc32b1b8530c3c8ed430032a
Luke San Antonio <lukesanantonio@gmail.com>
2015-04-12 18:03:19 -0400

    The share_object_*_ownership functions now have defined semantics when the passed in object is a pointer (not owned).
    The functionality is being tested!

--- src/tests/object.cpp
+++ src/tests/object.cpp
@@ -13,39 +13,70 @@ TEST_CASE("Object moving and sharing works as expected", "[struct Object]")
   using namespace strat::gfx;
 
   Object obj;
-
-  obj.mesh.set_owned(new Mesh());
-  obj.material.set_owned(new Material());
+  Object other;
 
   // We just need something unique to check later.
   obj.model_matrix = glm::mat4(10.0);
 
-  SECTION("Keep ownership")
+  SECTION("Original object owns it's data")
   {
-    auto other = share_object_keep_ownership(obj);
+    obj.mesh.set_owned(new Mesh());
+    obj.material.set_owned(new Material());
 
-    REQUIRE(other.mesh.is_pointer());
-    REQUIRE(obj.mesh.is_owned());
-    REQUIRE(other.mesh.get() == obj.mesh.get());
+    SECTION("Keep ownership")
+    {
+      other = share_object_keep_ownership(obj);
 
-    REQUIRE(other.material.is_pointer());
-    REQUIRE(obj.material.is_owned());
-    REQUIRE(other.material.get() == obj.material.get());
+      REQUIRE(other.mesh.is_pointer());
+      REQUIRE(obj.mesh.is_owned());
 
-    REQUIRE(other.model_matrix == obj.model_matrix);
+      REQUIRE(other.material.is_pointer());
+      REQUIRE(obj.material.is_owned());
+
+    }
+    SECTION("Move ownership")
+    {
+      other = share_object_move_ownership(obj);
+
+      REQUIRE(other.mesh.is_owned());
+      REQUIRE(obj.mesh.is_pointer());
+
+      REQUIRE(other.material.is_owned());
+      REQUIRE(obj.material.is_pointer());
+    }
   }
-  SECTION("Move ownership")
+  SECTION("Original data doesn't own it's data")
   {
-    auto other = share_object_move_ownership(obj);
+    auto mesh_data = std::make_unique<Mesh>();
+    auto mat_data = std::make_unique<Material>();
 
-    REQUIRE(other.mesh.is_owned());
-    REQUIRE(obj.mesh.is_pointer());
-    REQUIRE(other.mesh.get() == obj.mesh.get());
+    obj.mesh.set_pointer(mesh_data.get());
+    obj.material.set_pointer(mat_data.get());
 
-    REQUIRE(other.material.is_owned());
-    REQUIRE(obj.material.is_pointer());
-    REQUIRE(other.material.get() == obj.material.get());
+    SECTION("Keep pointer-ship")
+    {
+      other = share_object_keep_ownership(obj);
 
-    REQUIRE(other.model_matrix == obj.model_matrix);
+      REQUIRE(other.mesh.is_pointer());
+      REQUIRE(obj.mesh.is_pointer());
+
+      REQUIRE(other.material.is_pointer());
+      REQUIRE(obj.material.is_pointer());
+
+    }
+    SECTION("Move pointership")
+    {
+      other = share_object_move_ownership(obj);
+
+      REQUIRE(other.mesh.is_pointer());
+      REQUIRE(obj.mesh.is_pointer());
+
+      REQUIRE(other.material.is_pointer());
+      REQUIRE(obj.material.is_pointer());
+    }
   }
+
+  REQUIRE(other.mesh.get() == obj.mesh.get());
+  REQUIRE(other.material.get() == obj.material.get());
+  REQUIRE(other.model_matrix == obj.model_matrix);
 }
