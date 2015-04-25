892dc574691e71cbab43f09c36e85a3cd0cfce6b
Luke San Antonio <lukesanantonio@gmail.com>
2015-04-25 13:23:20 -0400

    Added a cmake module that will install svg assets as pngs using imagemagick.
    Also we now have a wrench for the UI!

---
+++ cmake/Modules/convert_files.cmake
@@ -0,0 +1,20 @@
+
+# Copyright (C) 2014 Luke San Antonio
+# All rights reserved.
+
+function(convert_to_png target dir in_file out_file)
+  # Create necessary directories
+  add_custom_command(TARGET ${target} PRE_BUILD COMMAND
+                     ${CMAKE_COMMAND} -E make_directory
+                     ${CMAKE_CURRENT_BINARY_DIR}/${dir})
+
+  # Find imagemagick
+  find_package(ImageMagick COMPONENTS convert REQUIRED)
+  # Actually convert the image.
+  add_custom_command(TARGET ${target} POST_BUILD COMMAND
+                     ${ImageMagick_convert_EXECUTABLE}
+                     -background none
+                     ${CMAKE_CURRENT_SOURCE_DIR}/${dir}/${in_file}
+                     -format png ${out_file}
+                     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${dir})
+endfunction()
