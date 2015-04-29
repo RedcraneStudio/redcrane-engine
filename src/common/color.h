520bd2694c011e3707d393634b3e7253e1fb90e3
Luke San Antonio <lukesanantonio@gmail.com>
2015-04-28 21:29:53 -0400

    Color structure now has an alpha component.

--- src/common/color.h
+++ src/common/color.h
@@ -8,14 +8,23 @@ namespace game
 {
   struct Color
   {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
+    // CTs WIN!
+    using c_t = uint8_t;
+
+    constexpr Color(c_t r = 0xff, // <- Default to some weird salmon color so
+                    c_t g = 0x8f, // we know when the client forgot to
+                    c_t b = 0xab, // initialize it's color!
+                    c_t a = 0xff) noexcept : r(r), g(g), b(b), a(a) {}
+
+    c_t r;
+    c_t g;
+    c_t b;
+    c_t a;
   };
 
   inline bool operator==(Color const& c1, Color const& c2) noexcept
   {
-    return c1.r == c2.r && c1.g == c2.g && c1.b == c2.b;
+    return c1.r == c2.r && c1.g == c2.g && c1.b == c2.b && c1.a == c2.a;
   }
   inline bool operator!=(Color const& c1, Color const& c2) noexcept
   {
@@ -24,10 +33,10 @@ namespace game
 
   namespace colors
   {
-    constexpr Color white {0xff, 0xff, 0xff};
-    constexpr Color black {0x00, 0x00, 0x00};
-    constexpr Color red   {0xff, 0x00, 0x00};
-    constexpr Color green {0x00, 0xff, 0x00};
-    constexpr Color blue  {0x00, 0x00, 0xff};
+    constexpr Color white {0xff, 0xff, 0xff, 0xff};
+    constexpr Color black {0x00, 0x00, 0x00, 0xff};
+    constexpr Color red   {0xff, 0x00, 0x00, 0xff};
+    constexpr Color green {0x00, 0xff, 0x00, 0xff};
+    constexpr Color blue  {0x00, 0x00, 0xff, 0xff};
   }
 }
