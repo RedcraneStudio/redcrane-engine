dfb215fa375843eb4a4fc3145e94337e15236875
Luke San Antonio <lukesanantonio@gmail.com>
2015-05-02 11:48:11 -0400

    Software_Mesh now learned a child/impl, it's more of a decorator now.

--- src/common/software_mesh.cpp
+++ src/common/software_mesh.cpp
@@ -5,17 +5,36 @@
 #include "software_mesh.h"
 namespace game
 {
+  void Software_Mesh::set_impl(Mesh& m, bool should_prepare) noexcept
+  {
+    // If we were prepared with a mesh, we should prepare this child but only
+    // if the user specifially told us to do that.
+    if(prepared_ && should_prepare)
+    {
+      copy_to(m);
+    }
+
+    impl_ = &m;
+  }
   void Software_Mesh::prepare(Mesh_Data const& data) noexcept
   {
+    prepared_ = true;
     mesh_data_ = data;
+    if(impl_) impl_->prepare(mesh_data_);
   }
   void Software_Mesh::prepare(Mesh_Data&& data) noexcept
   {
+    prepared_ = true;
+
     mesh_data_ = std::move(data);
+    // Purposely copy into our child, since we want to maintain ownership to
+    // the mesh data.
+    if(impl_) impl_->prepare(mesh_data_);
   }
   void Software_Mesh::set_vertices(unsigned int begin, unsigned int length,
                                    Vertex const* src_vertices) noexcept
   {
     mesh_data_.set_vertices(begin, length, src_vertices);
+    if(impl_) impl_->set_vertices(begin, length, src_vertices);
   }
 }
--- src/common/software_mesh.h
+++ src/common/software_mesh.h
@@ -7,11 +7,23 @@
 namespace game
 {
   /*!
-   * \brief A simple mesh impl that wraps a mesh_data.
+   * \brief A simple decorater that stores mesh data for access.
    */
   struct Software_Mesh : public Mesh
   {
     virtual ~Software_Mesh() noexcept {}
+
+    /*!
+     * \brief Sets the child of this mesh.
+     *
+     * \params should_prepare if true, this function will copy it's mesh data
+     * to the given child if it has been prepared itself. The child will be
+     * prepared after all subsequent calls to this structures prepare method
+     * anyway.
+     */
+    void set_impl(Mesh& m, bool should_prepare = false) noexcept;
+    Mesh& get_impl() noexcept { return *impl_; }
+    Mesh const& get_impl() const noexcept { return *impl_; }
 
     void prepare(Mesh_Data const& data) noexcept override;
     void prepare(Mesh_Data&& data) noexcept override;
@@ -24,8 +36,15 @@ namespace game
     inline Mesh_Data&& unwrap_mesh_data() noexcept
     { return std::move(mesh_data_); }
 
+    /*!
+     * \brief Constructs a new mesh with a copy of its own mesh data.
+     *
+     * This function does not depend on an implementation / child.
+     */
     inline void copy_to(Mesh& m) const noexcept { m.prepare(mesh_data_); }
   private:
+    Mesh* impl_;
+    bool prepared_ = false;
     Mesh_Data mesh_data_;
   };
 }
