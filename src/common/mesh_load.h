25c1413658a456118472ff861d86e6ca0548b7dc
Luke San Antonio <lukesanantonio@gmail.com>
2015-05-02 11:06:06 -0400

    Added a bit of interface clarification to the load_obj function.
    Hopefully it means something.

--- src/common/mesh_load.h
+++ src/common/mesh_load.h
@@ -7,5 +7,8 @@
 #include "mesh.h"
 namespace game
 {
+  // The reason this function accepts a mesh impl and doesn't just return mesh
+  // data is because error handling is as easy as not preparing the mesh, then
+  // again it will go unnoticed. That being said an empty mesh could do fine.
   void load_obj(std::string obj, Mesh& m) noexcept;
 }
