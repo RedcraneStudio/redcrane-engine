7467c83f7c6151afdeb57aec9bea1ddbbce4cccd
Luke San Antonio <lukesanantonio@gmail.com>
2015-05-26 15:27:06 -0400

    We should be able to use easily generate and update a heightmap from noise.

---
+++ src/map/water.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#include "water.h"
+#include "../common/noise.h"
+#include "map.h"
+namespace game
+{
+  void set_noise_heightmap(Heightmap& h, double time) noexcept
+  {
+    for(int i = 0; i < h.extents.y * h.extents.y; ++i)
+    {
+      int x = i % h.extents.x;
+      int y = i / h.extents.x;
+
+      h.vals[i] = perlin_noise(x / 10.0f, y / 10.0f, time) * 100;
+    }
+  }
+}
---
+++ src/map/water.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+namespace game
+{
+  struct Heightmap;
+  void set_noise_heightmap(Heightmap&, double time) noexcept;
+}
