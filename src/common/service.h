9873a8f1248161221e7f62755f91adc2d5437d97
Luke San Antonio <lukesanantonio@gmail.com>
2015-06-12 12:56:22 -0400

    We now have a basic service locator that may or may not be used.

---
+++ src/common/service.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+#include <memory>
+namespace game
+{
+  template <class T>
+  struct Service
+  {
+    static T* get() noexcept;
+    static void set(std::unique_ptr<T>) noexcept;
+
+    static std::unique_ptr<T> ptr;
+  };
+
+  template <class T>
+  T* Service<T>::get() noexcept
+  {
+    return ptr.get();
+  };
+
+  template <class T>
+  void Service<T>::set(std::unique_ptr<T> np) noexcept
+  {
+    ptr = std::move(np);
+  }
+}
