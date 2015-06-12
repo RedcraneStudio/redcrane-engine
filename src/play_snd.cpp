6a239f42a1faa2a91e733f58f29271c77558afc8
Luke San Antonio <lukesanantonio@gmail.com>
2015-06-11 21:42:01 -0400

    The play_snd executable now works as expected.

--- src/play_snd.cpp
+++ src/play_snd.cpp
@@ -18,50 +18,31 @@ int main(int argc, char** argv)
 
   Scoped_Log_Init log_init{};
 
-  int device = 0;
-
-
-  if(argc < 4)
+  if(argc < 2)
   {
     // Fuck you, user
-    log_e("Pass in file, time, and device, faggot!");
-
-
-    snd::initialize_pa();
-
-    // Print available devices.
-    int device_count = Pa_GetDeviceCount();
-    for(int i = 0; i < device_count; ++i)
-    {
-      auto device_info = Pa_GetDeviceInfo(i);
-      log_e("Device %) %", i + 1, device_info->name);
-    }
-
-    snd::terminate_pa();
+    log_e("Pass in a file!");
     return 1;
-  }
-  else
-  {
-    device = std::atoi(argv[3]);
-    if(device == 0)
-    {
-      log_e("Bad device number!");
-      return 1;
-    }
-    device -= 1;
   }
 
   snd::initialize_pa();
 
   {
-    auto stream = snd::Stream{device};
+    auto stream = snd::Stream{};
 
     auto flac = snd::load_flac(argv[1]);
+
+#if 0
+    auto f = fopen("data.pcm", "wb");
+    for(auto i : flac.samples)
+    {
+      fwrite(&i.left, sizeof(i), 1, f);
+    }
+#endif
+
     stream.use_pcm(flac);
 
     stream.start();
-    std::this_thread::sleep_for(std::chrono::seconds(std::atoi(argv[2])));
-    stream.stop(true);
   }
 
   snd::terminate_pa();
