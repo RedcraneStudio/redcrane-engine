2e578ef94bcf97318d037b8e66ab7add19d8ca31
Luke San Antonio <lukesanantonio@gmail.com>
2015-06-19 17:45:21 -0400

    Added a heightmap like class template for arbitrary storage of data per map location.

---
+++ src/stratlib/value_map.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+#include "../common/vec.h"
+namespace game { namespace strat
+{
+  template <class T>
+  struct Value_Map
+  {
+    void allocate(Vec<int> const& extents) noexcept;
+    ~Value_Map() noexcept;
+
+    T* values = nullptr;
+    Vec<int> extents = Vec<int>{0,0};
+    bool allocated = false;
+  };
+
+  template <class T>
+  void Value_Map<T>::allocate(Vec<int> const& e) noexcept
+  {
+    values = new T[area(e)];
+    extents = e;
+    allocated = true;
+  }
+  template <class T>
+  Value_Map<T>::~Value_Map() noexcept
+  {
+    delete[] values;
+  }
+} }
