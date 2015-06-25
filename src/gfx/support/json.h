9932a2d27962e0889d9275300541aa5f3483e8d1
Luke San Antonio <lukesanantonio@gmail.com>
2015-06-25 09:18:00 -0400

    Added a function to convert a json value to a vec3.

---
+++ src/gfx/support/json.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+#include "rapidjson/document.h"
+#include "glm/glm.hpp"
+namespace game
+{
+  inline glm::vec3 vec3_from_js(rapidjson::Value const& v) noexcept
+  {
+    return glm::vec3{v["x"].GetDouble(),v["y"].GetDouble(),v["z"].GetDouble()};
+  }
+}
