12bf983197eaf75700f1923c3fcee62835691d0c
Luke San Antonio <lukesanantonio@gmail.com>
2015-07-17 22:27:43 -0400

    We are (inefficiently) checking collision between structures.

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -3,6 +3,8 @@
  * All rights reserved.
  */
 #include "map.h"
+#include "../collisionlib/sweep_and_prune.h"
+#include "../common/log.h"
 
 namespace game
 {
@@ -21,6 +23,34 @@ namespace game
   bool try_structure_place(Map& map, Structure const& st, pos_t pos) noexcept
   {
     // Check collision with every other structure.
+    SAP sap;
+    std::vector<SAP_AABB_Ref> ids;
+    for(auto const& other_st : map.structures)
+    {
+      auto aabb = other_st.structure().aabb();
+      aabb.min.x += other_st.position.x;
+      aabb.min.z += other_st.position.y;
+      aabb.min.y = 0.0f;
+      ids.push_back(sap.insert(aabb));
+    }
+
+    auto aabb = st.aabb();
+    aabb.min.x += pos.x;
+    aabb.min.z += pos.y;
+    aabb.min.y = 0.0f;
+    SAP_AABB_Ref this_id = sap.insert(aabb);
+
+    auto pairs = sap.collisions();
+
+    for(auto pair : pairs)
+    {
+      if(pair.first == this_id || pair.second == this_id)
+      {
+        // That's a collision
+        return false;
+      }
+      // Otherwise let it go through.
+    }
 
     // Make sure it is in the bounds of the map
 
