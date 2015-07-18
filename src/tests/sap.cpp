3cbdc89f2a0008e226e9e240c6c3033eaedb50f4
Luke San Antonio <lukesanantonio@gmail.com>
2015-07-17 22:27:30 -0400

    More collision tests.

--- src/tests/sap.cpp
+++ src/tests/sap.cpp
@@ -74,3 +74,57 @@ TEST_CASE("SAP algorithm detects AABB collision", "[SAP]")
   REQUIRE(pairs_equal(result[2], std::make_pair(id4, id2)));
   REQUIRE(pairs_equal(result[3], std::make_pair(id2, id3)));
 }
+TEST_CASE("MO TESTS", "[SAP]")
+{
+  using namespace game;
+
+  auto pairs_equal = [](auto const& fp, auto const& sp)
+  {
+    return (std::get<0>(fp) == std::get<0>(sp) &&
+            std::get<1>(fp) == std::get<1>(sp)) ||
+           (std::get<0>(fp) == std::get<1>(sp) &&
+            std::get<1>(fp) == std::get<0>(sp));
+  };
+
+  SAP sap;
+
+  SECTION("Test #1")
+  {
+    // (9.218991, 0.000000, 2.445128), 2.339422, 1.333736, 2.339422
+    // (7.690152, 0.000000, 3.882454), 2.339422, 1.333736, 2.339422
+
+    AABB a1{2.339422, 1.333736, 2.339422, {9.218991, 0.0, 2.445128}};
+    AABB a2{2.339422, 1.333736, 2.339422, {7.690152, 0.0, 3.882454}};
+
+    auto id1 = sap.insert(a1);
+    auto id2 = sap.insert(a2);
+
+    auto result = sap.collisions();
+    REQUIRE(result.size() == 1);
+
+    // We shouldn't depend on the order but for now this is just how it's going
+    // to go :)
+    REQUIRE(pairs_equal(result[0], std::make_pair(id1, id2)));
+  }
+  SECTION("Test #2")
+  {
+    // (16.080256, 0.000000, -38.153481), 2.339422, 1.333736, 2.339422
+    // (4.690701,  0.000000,   5.771526), 2.339422, 1.333736, 2.339422
+    // (5.051381,  0.000000,   5.521357), 2.339422, 1.333736, 2.339422
+
+    AABB a1{2.339422, 1.333736, 2.339422, {16.080256,0.0, -37.153481}};
+    AABB a2{2.339422, 1.333736, 2.339422, {4.690701, 0.0, 5.771526}};
+    AABB a3{2.339422, 1.333736, 2.339422, {5.051381, 0.0, 5.521357}};
+
+    auto id1 = sap.insert(a1);
+    auto id2 = sap.insert(a2);
+    auto id3 = sap.insert(a3);
+
+    auto result = sap.collisions();
+    REQUIRE(result.size() == 1);
+
+    // We shouldn't depend on the order but for now this is just how it's going
+    // to go :)
+    REQUIRE(pairs_equal(result[0], std::make_pair(id2, id3)));
+  }
+}
