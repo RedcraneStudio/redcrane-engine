0b65adb4c00d682fd507d3944d944c7ced19c9d8
Luke San Antonio <lukesanantonio@gmail.com>
2015-08-19 18:00:08 -0400

    Add gfx support functions to render normals of mesh data
    It uses the Immediate_Renderer.

---
+++ src/gfx/support/render_normals.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio and Hazza Alkaabi
+ * All rights reserved.
+ */
+#include "render_normals.h"
+namespace game { namespace gfx
+{
+  void render_normals(Immediate_Renderer& r, Indexed_Mesh_Data const& m,
+                      glm::mat4 const& model) noexcept
+  {
+    for(auto i : m.elements)
+    {
+      auto vertex = m.vertices[i];
+
+      auto position = model * glm::vec4(vertex.position, 1.0f);
+      auto normal = model * glm::vec4(vertex.normal, 0.0f);
+
+      r.draw_line(glm::vec3(position), glm::vec3(position + normal));
+    }
+  }
+  void render_normals(Immediate_Renderer& r, Ordered_Mesh_Data const& m,
+                      glm::mat4 const& model) noexcept
+  {
+    for(auto vertex : m.vertices)
+    {
+      auto position = model * glm::vec4(vertex.position, 1.0f);
+      auto normal = model * glm::vec4(vertex.normal, 0.0f);
+      r.draw_line(glm::vec3(position), glm::vec3(position + normal));
+    }
+  }
+  void render_normals(Immediate_Renderer& r,
+                      Indexed_Split_Mesh_Data const& m,
+                      glm::mat4 const& model) noexcept
+  {
+    for(auto vert_ref : m.indices)
+    {
+      auto position = model * glm::vec4(m.positions[*vert_ref.position], 1.0f);
+      auto normal = model * glm::vec4(m.normals[*vert_ref.normal], 0.0f);
+      r.draw_line(glm::vec3(position), glm::vec3(position + normal));
+    }
+  }
+  void render_normals(Immediate_Renderer& r,
+                      Ordered_Split_Mesh_Data const& m,
+                      glm::mat4 const& model) noexcept
+  {
+    auto size = std::min(m.positions.size(), m.normals.size());
+    for(int i = 0; i < size; ++i)
+    {
+      auto position = model * glm::vec4(m.positions[i], 1.0f);
+      auto normal = model * glm::vec4(m.normals[i], 0.0f);
+      r.draw_line(glm::vec3(position), glm::vec3(position + normal));
+    }
+  }
+} }
---
+++ src/gfx/support/render_normals.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio and Hazza Alkaabi
+ * All rights reserved.
+ */
+#pragma once
+#include "../immediate_renderer.h"
+#include "../mesh_data.h"
+namespace game { namespace gfx
+{
+  void render_normals(Immediate_Renderer&, Indexed_Mesh_Data const&,
+                      glm::mat4 const&) noexcept;
+  void render_normals(Immediate_Renderer&, Ordered_Mesh_Data const&,
+                      glm::mat4 const&) noexcept;
+  void render_normals(Immediate_Renderer&, Indexed_Split_Mesh_Data const&,
+                      glm::mat4 const&) noexcept;
+  void render_normals(Immediate_Renderer&, Ordered_Split_Mesh_Data const&,
+                      glm::mat4 const&) noexcept;
+} }
