9f1400aba7435db97111087d35ea70a15b9d70e2
Luke San Antonio <lukesanantonio@gmail.com>
2015-08-26 02:50:54 -0400

    Add a new lua-integration lib into the src tree

---
+++ src/luaint/common.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio and Hazza Alkaabi
+ * All rights reserved.
+ */
+#include "common.h"
+namespace game { namespace luaint
+{
+} }
---
+++ src/luaint/common.h
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio and Hazza Alkaabi
+ * All rights reserved.
+ */
+#pragma once
+namespace game { namespace luaint
+{
+} }
