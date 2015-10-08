f1d75af1f36cf9e308331b5708c9f4db3cbbcf49
Luke San Antonio <lukesanantonio@gmail.com>
2015-10-07 21:53:49 -0400

    New tests/ subtree
    Hopefully this one will be (slightly) less painful to use and we'll be writing
    more tests soon.

---
+++ tests/common/tree.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+
+#include "catch/catch.hpp"
+
+TEST_CASE("", "[Tree]")
+{
+}
---
+++ tests/main.cpp
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "catch/catch.hpp"
