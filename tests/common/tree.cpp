d74b27e1db6dcb1f68bf12cc685847180af464bc
Luke San Antonio <lukesanantonio@gmail.com>
2015-10-08 19:12:23 -0400

    Add more tests for the tree template

--- tests/common/tree.cpp
+++ tests/common/tree.cpp
@@ -41,4 +41,45 @@ TEST_CASE("tree returns valid iterators for allocated depth levels", "[Tree]")
       REQUIRE(tree.level_begin(2)->val == expect);
     }
   }
+
+  SECTION("iterators to begin and end of level work")
+  {
+    // The iterator to the beginning of the first level should definitely be
+    // equal to the very beginning of the tree.
+    REQUIRE(tree.level_begin(0) == tree.begin());
+
+    // Level zero always contains a single node.
+    REQUIRE(tree.level_end(0) == tree.begin()+1);
+
+    // For a quadtree, the second level should start at 1 and end at 5
+    REQUIRE(tree.level_begin(1) == tree.begin()+1);
+    REQUIRE(tree.level_end(1) == tree.begin()+5);
+
+    REQUIRE(tree.level_begin(2) == tree.begin()+5);
+    REQUIRE(tree.level_end(2) == tree.begin()+21);
+  }
+  SECTION("indexing works as expected")
+  {
+    constexpr int expect = 3;
+
+    tree.node_at_depth(2, 0).val = expect;
+    REQUIRE(tree.level_begin(2)->val == expect);
+
+    tree.node_at_depth(0, 0).val = expect;
+    REQUIRE(tree.level_begin(0)->val == expect);
+
+    tree.node_at_depth(1, 1).val = expect;
+    REQUIRE((tree.begin()+2)->val == expect);
+
+    // This is pretty weird usage, but might be found in the wild
+    tree.node_at_depth(0, 1).val = expect;
+    REQUIRE(tree.level_begin(1)->val == expect);
+    REQUIRE((tree.level_begin(0) + 1)->val == expect);
+
+    // Test that same usage in a different way.
+    tree.node_at_depth(1, 4).val = expect;
+    REQUIRE(tree.level_begin(2)->val == expect);
+    REQUIRE((tree.level_begin(1) + 4)->val == expect);
+    REQUIRE((tree.level_begin(0) + 5)->val == expect);
+  }
 }
