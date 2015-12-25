d6aeae723e0ac7e2d6ce9eeb66b71de526841080
Luke San Antonio <lukesanantonio@gmail.com>
2015-12-25 15:52:59 -0500

    Add CMake to find ENEt

---
+++ cmake/Modules/FindENet.cmake
@@ -0,0 +1,10 @@
+
+# Copyright (C) 2015 Luke San Antonio
+# All rights reserved.
+
+find_path(ENet_INCLUDE_DIR enet/enet.h)
+find_library(ENet_LIBRARY FLAC)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ENet DEFAULT_MSG ENet_INCLUDE_DIR ENet_LIBRARY)
+
