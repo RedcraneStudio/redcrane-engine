a5bbcea53e4165799ffe19efe640875ce35da4c1
Luke San Antonio <lukesanantonio@gmail.com>
2015-12-26 14:07:13 -0500

    uselib: Add Mesh_Cache to avoid overuse of IDriver

---
+++ src/use/mesh_cache.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#include "mesh_cache.h"
+namespace game { namespace gfx
+{
+  Mesh_Chunk Mesh_Cache::load_owned_chunk(std::string filename) noexcept
+  {
+    return load_mesh(*driver_, {filename}).chunk;
+  }
+} }
+
---
+++ src/use/mesh_cache.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+#include "mesh.h"
+namespace game { namespace gfx
+{
+  /*!
+   * \brief Wraps mesh creation without a need to pass the driver.
+   *
+   * The driver is really what is cached, so that it doesn't have to be
+   * liberally passed around everywhere like things have been.
+   */
+  struct Mesh_Cache
+  {
+    Mesh_Cache(IDriver& driver) noexcept : driver_(&driver) {}
+
+    Mesh_Chunk load_owned_chunk(std::string filename) noexcept;
+  private:
+    IDriver* driver_;
+  };
+} }
