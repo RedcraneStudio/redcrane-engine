3e9e0e75bfa57469d8e72b13d00ca42e6ce2bb68
Luke San Antonio <lukesanantonio@gmail.com>
2016-01-11 11:23:09 -0500

    Add task ABC
    This may be able to be used to abstract the client, local server and dedicated
    server operating modes.

---
+++ src/common/task.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#include "task.h"
+#include <algorithm>
+namespace redc
+{
+  Task_Composite::Task_Composite(std::initializer_list<task_t> init) noexcept
+  {
+    tasks_.resize(init.size());
+    std::move(init.begin(), init.end(), tasks_.begin());
+  }
+  void Task_Composite::step() noexcept
+  {
+    for(auto& task : tasks_)
+    {
+      task->step();
+    }
+  }
+}
+
---
+++ src/common/task.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+#include <initializer_list>
+#include <vector>
+#include "maybe_owned.hpp"
+namespace redc
+{
+  struct Task
+  {
+    virtual ~Task() noexcept {}
+    virtual void step() noexcept;
+  };
+
+  struct Task_Composite : public Task
+  {
+    using task_t = Maybe_Owned<Task>;
+
+    Task_Composite(std::initializer_list<task_t> init) noexcept;
+
+    void step() noexcept override;
+  private:
+    std::vector<task_t> tasks_;
+  };
+}
