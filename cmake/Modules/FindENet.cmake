32cb6dd00dc0ae4d60a61786256ffc959cf41cb2
Luke San Antonio <lukesanantonio@gmail.com>
2016-02-06 21:20:51 -0500

    FindENet.cmake: Look for Enet not Flac

--- cmake/Modules/FindENet.cmake
+++ cmake/Modules/FindENet.cmake
@@ -3,7 +3,7 @@
 # All rights reserved.
 
 find_path(ENet_INCLUDE_DIR enet/enet.h)
-find_library(ENet_LIBRARY FLAC)
+find_library(ENet_LIBRARY enet)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ENet DEFAULT_MSG ENet_INCLUDE_DIR ENet_LIBRARY)
