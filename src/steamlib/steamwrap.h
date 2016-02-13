3c34cd73ca904465aa53a7cb57e6220d281a23f8
Luke San Antonio <lukesanantonio@gmail.com>
2016-02-13 13:58:02 -0500

    Add steamlib for wrapping steamworks sdk

---
+++ src/steamlib/steamwrap.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2015 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+namespace redc { namespace steam
+{
+  struct Scoped_Steam_Init
+  {
+    Scoped_Steam_Init();
+    ~Scoped_Steam_Init();
+  };
+} }
