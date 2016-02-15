1454f5a15531eee57c6c9cfd073b4e6495e96346
Luke San Antonio <lukesanantonio@gmail.com>
2016-02-15 17:04:25 -0500

    Add assertion code that checks for exceptions

--- src/common/debugging.h
+++ src/common/debugging.h
@@ -3,7 +3,7 @@
  * All rights reserved.
  */
 #pragma once
-
+#include <string>
 #ifndef NDEBUG
   #define REDC_DEBUGGING_ENABLED
 
@@ -16,6 +16,19 @@
   // Don't do anything stupid like count on side effects of the condition.
   #define REDC_ASSERT(condition) \
     ::redc::assert_fn((condition), #condition, __FILE__, __LINE__)
+
+  #define REDC_ASSERT_NO_THROW(expr) \
+    try { expr; } \
+    catch(std::exception& e) \
+    { \
+      log_e("% thrown by expression. Crashing", e.what()); \
+      ::redc::assert_fn(false, #expr, __FILE__, __LINE__); \
+    } \
+    catch(...) \
+    { \
+      log_e("unknown exception thrown by expression. Crashing"); \
+      ::redc::assert_fn(false, #expr, __FILE__, __LINE__); \
+    } \
 
 #else
   #define REDC_ASSERT(condition) ((void)0)
