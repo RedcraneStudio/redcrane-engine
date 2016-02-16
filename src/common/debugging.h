1b4e371f7e22aa69f1020ab7279c8b33ceb2f007
Luke San Antonio <lukesanantonio@gmail.com>
2016-02-15 21:50:45 -0500

    New debugging macro / assertion implementation
    Support logging a custom message and simplify implementation

--- src/common/debugging.h
+++ src/common/debugging.h
@@ -3,33 +3,58 @@
  * All rights reserved.
  */
 #pragma once
-#include <string>
+#include "log.h"
 #ifndef NDEBUG
   #define REDC_DEBUGGING_ENABLED
+#endif
 
   namespace redc
   {
+    template <class... Msg_Args>
     void assert_fn(bool val, char const* const condition,
-                   char const* const filename, long line) noexcept;
+                   char const* const filename, int long line,
+                   Msg_Args&&... args)
+    {
+      if(!val)
+      {
+        redc::log_e(std::forward<Msg_Args>(args)...);
+        redc::log_e("Assertion '%' failed in %:%", condition, filename, line);
+
+        // If we *are* debugging, actually abort and flush the log so we know
+        // the messages above will show up.
+#ifdef REDC_DEBUGGING_ENABLED
+        redc::flush_log_full();
+        std::abort();
+#endif
+      }
+    }
   }
 
-  // Don't do anything stupid like count on side effects of the condition.
-  #define REDC_ASSERT(condition) \
-    ::redc::assert_fn((condition), #condition, __FILE__, __LINE__)
+// Don't do anything stupid like count on side effects of the condition.
+#define REDC_ASSERT(condition) \
+  ::redc::assert_fn((condition), #condition, __FILE__, __LINE__)
 
-  #define REDC_ASSERT_NO_THROW(expr) \
-    try { expr; } \
-    catch(std::exception& e) \
-    { \
-      log_e("% thrown by expression. Crashing", e.what()); \
-      ::redc::assert_fn(false, #expr, __FILE__, __LINE__); \
-    } \
-    catch(...) \
-    { \
-      log_e("unknown exception thrown by expression. Crashing"); \
-      ::redc::assert_fn(false, #expr, __FILE__, __LINE__); \
-    } \
+/*
+ * This passes everything but the first argument verbatim to the log formatting
+ * function. The cool thing about doing it this way is that we use macros to
+ * stringify the condition as well as provided an evaluation of that
+ * problematic expression which is probably necessary. For example when
+ * crashing due to a bad filename, it is very important to provide that
+ * filename in the message.
+ */
+#define REDC_ASSERT_MSG(condition, ...) \
+  ::redc::assert_fn((condition), #condition, __FILE__, __LINE__, \
+                    __VA_ARGS__)
 
-#else
-  #define REDC_ASSERT(condition) ((void)0)
-#endif
+#define REDC_ASSERT_NO_THROW(expr) \
+  try { expr; } \
+  catch(std::exception& e) \
+  { \
+    ::redc::log_e("% thrown by expression. Crashing", e.what()); \
+    ::redc::assert_fn(false, #expr, __FILE__, __LINE__); \
+  } \
+  catch(...) \
+  { \
+    ::redc::log_e("unknown exception thrown by expression. Crashing"); \
+    ::redc::assert_fn(false, #expr, __FILE__, __LINE__); \
+  }
