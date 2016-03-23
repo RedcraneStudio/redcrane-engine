3c8012b6cf6bc00e27e872da01260d9b9d40fa5d
Luke San Antonio <lukesanantonio@gmail.com>
2016-03-23 15:50:46 -0400

    Add some code towards a bullet character controller

--- src/player.cpp
+++ src/player.cpp
@@ -2,4 +2,71 @@
 
 namespace redc
 {
+  Player_Controller::Player_Controller() : inited_(false),
+                                           ghost_(),
+                                           shape_(.25f, 1.8f) {}
+
+  // They are expected to add the action to the dynamics world and then this
+  // will be called, then we init then we start
+  void Player_Controller::updateAction(btCollisionWorld* world, btScalar dt)
+  {
+    if(!inited_)
+    {
+      btTransform trans;
+      trans.setIdentity();
+      trans.setOrigin(btVector3(0.0f, .5f, 0.0f));
+      ghost_.setWorldTransform(trans);
+      ghost_.setCollisionShape(&shape_);
+      world->addCollisionObject(&ghost_);
+      ghost_.setCollisionFlags(ghost_.getCollisionFlags() |
+                               btCollisionObject::CF_KINEMATIC_OBJECT);
+      ghost_.setActivationState(DISABLE_DEACTIVATION);
+      inited_ = true;
+      return;
+    }
+
+    // Recover from intersection
+    btVector3 min_aabb, max_aabb;
+    shape_.getAabb(ghost_.getWorldTransform(), min_aabb, max_aabb);
+    world->getBroadphase()->setAabb(ghost_.getBroadphaseHandle(), min_aabb,
+                                    max_aabb, world->getDispatcher());
+    bool penetration = false;
+    world->getDispatcher()->dispatchAllCollisionPairs(
+            ghost_.getOverlappingPairCache(),
+            world->getDispatchInfo(),
+            world->getDispatcher());
+
+    auto cur_pos = ghost_.getWorldTransform().getOrigin();
+
+    btScalar maxPen = btScalar(0.0);
+
+    auto xform = ghost_.getWorldTransform();
+
+    if(input_ref_->forward)
+    {
+      auto orig = xform.getOrigin();
+      orig.setZ(orig.getZ() + .1f);
+      xform.setOrigin(orig);
+    }
+    if(input_ref_->backward)
+    {
+      auto orig = xform.getOrigin();
+      orig.setZ(orig.getZ() - .1f);
+      xform.setOrigin(orig);
+    }
+    if(input_ref_->strafe_left)
+    {
+      auto orig = xform.getOrigin();
+      orig.setX(orig.getX() - .1f);
+      xform.setOrigin(orig);
+    }
+    if(input_ref_->strafe_right)
+    {
+      auto orig = xform.getOrigin();
+      orig.setX(orig.getX() + .1f);
+      xform.setOrigin(orig);
+    }
+
+    ghost_.setWorldTransform(xform);
+  }
 }
--- src/player.h
+++ src/player.h
@@ -4,6 +4,8 @@
  */
 #pragma once
 #include <LinearMath/btMotionState.h>
+#include <BulletDynamics/btBulletDynamicsCommon.h>
+#include <BulletCollision/CollisionDispatch/btGhostObject.h>
 #include "SDL.h"
 #include "input/input.h"
 namespace redc
@@ -15,5 +17,25 @@ namespace redc
     // Get updated by the dynamics world
     void setWorldTransform(btTransform const& world) override;
     void getWorldTransform(btTransform& trans) const override;
+  };
+
+  struct Player_Controller : public btActionInterface
+  {
+    Player_Controller();
+
+    void updateAction(btCollisionWorld* world, btScalar dt) override;
+    void debugDraw(btIDebugDraw*) {}
+
+    inline void set_input_ref(Input* input) { input_ref_ = input; }
+    Input* get_input_ref() { return input_ref_; }
+
+    inline btTransform getWorldTransform(btTransform& t) const
+    { t = ghost_.getWorldTransform(); }
+  private:
+    bool inited_;
+    btPairCachingGhostObject ghost_;
+    btCapsuleShape shape_;
+
+    Input* input_ref_;
   };
 }
