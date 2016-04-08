bb07e9894a0a2ca83a209a7b039e87a4f820059f
Luke San Antonio <lukesanantonio@gmail.com>
2016-04-08 19:21:13 -0400

    Print the selected server mode in run_engine.lua

--- src/lua/run_engine.lua
+++ src/lua/run_engine.lua
@@ -2,6 +2,19 @@
 -- Copyright (C) 2016 Luke San Antonio
 -- All rights reserved.
 
+local rc = require("redcrane")
+
 local eng, server_mode, sandbox = ...
+
+if server_mode == "dedicated" then
+  rc:log_i("Starting dedicated server")
+elseif server_mode == "connect" then
+  rc:log_i("Connecting")
+elseif server_mode == "local" then
+  rc:log_i("Starting local server")
+else
+  rc:log_i("Invalid server mode '"..server_mode.."'")
+  return 1
+end
 
 return 0
