221bc0b01847566d9a2be9efc58306162f3068e0
Luke San Antonio <lukesanantonio@gmail.com>
2016-04-30 13:38:48 -0400

    Add observer_ptr
    From https://github.com/martinmoene/observer-ptr.

---
+++ src/common/observer_ptr.hpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2016 Luke San Antonio
+ * All rights reserved.
+ *
+ * Implementation of std::experimental::observer_ptr
+ */
+#pragma once
+#include "nonstd/observer_ptr.h"
+namespace redc
+{
+  using ::nonstd::observer_ptr;
+  using ::nonstd::make_observer;
+}
