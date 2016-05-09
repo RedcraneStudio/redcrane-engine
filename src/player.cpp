e210336b2e55d48d7f55836dd68297dc88dff392
Luke San Antonio <lukesanantonio@gmail.com>
2016-05-08 20:02:53 -0400

    Add better collision code to player controller
    This is mostly a direct copy from btKinematicCharacterController.

--- src/player.cpp
+++ src/player.cpp
@@ -39,42 +39,99 @@ namespace redc
 
       // Apply some sort of gravity
       orig.setY(orig.getY() - .05f);
-
       xform.setOrigin(orig);
     }
+    ghost_.setWorldTransform(xform);
 
-    auto cur_pos = xform.getOrigin();
-    auto end_pos = cur_pos;
+    // ===
+    // Inspired very heavily by the btKinematicCharacterController.
+    // See http://bulletphysics.org/Bullet/BulletFull/btKinematicCharacterController_8cpp_source.html
+    // for reference, license, etc.
+    // ===
 
-    // The player's height ought to be enough
-    end_pos.setY(cur_pos.getY() - PLAYER_CAPSULE_HEIGHT / 2.0f);
+    // Here we must refresh the overlapping paircache as the penetrating
+    // movement itself or the previous recovery iteration might have used
+    // setWorldTransform and pushed us into an object that is not in the
+    // previous cache contents from the last timestep, as will happen if we are
+    // pushed into a new AABB overlap. Unhandled this means the next convex
+    // sweep gets stuck.
 
-    // Perform a raytest from the character to the ground and lock it there.
-    btCollisionWorld::ClosestRayResultCallback ray_cb(cur_pos, end_pos);
-    world->rayTest(cur_pos, end_pos, ray_cb);
-    if(ray_cb.hasHit())
-    {
-      auto end = ray_cb.m_hitPointWorld;
+    // Do this by calling the broadphase's setAabb with the moved AABB, this
+    // will update the broadphase paircache and the ghostobject's internal
+    // paircache at the same time. /BW
 
-      // Put it right on top!
-      end.setY(end.getY() + PLAYER_CAPSULE_HEIGHT / 2.0f);
-      xform.setOrigin(end);
-    }
+    btVector3 minAabb, maxAabb;
+    shape_.getAabb(ghost_.getWorldTransform(), minAabb, maxAabb);
+    world->getBroadphase()->setAabb(ghost_.getBroadphaseHandle(),
+                                    minAabb, maxAabb, world->getDispatcher());
 
-#if 0
-    // Recover from intersection
-    btVector3 min_aabb, max_aabb;
-    shape_.getAabb(ghost_.getWorldTransform(), min_aabb, max_aabb);
-    world->getBroadphase()->setAabb(ghost_.getBroadphaseHandle(), min_aabb,
-                                    max_aabb, world->getDispatcher());
+
+    // Added Notes:
+    // The ghost object already knows what it's colliding with but doesn't have
+    // any collision manifolds (lists of intersecting points between a pair of
+    // objects). Basically, the ghost pair cache and the world pair cache are
+    // kept mostly up to date thanks to the btGhostPairCallback set in the
+    // world's pair cache above, but the pair objects (and algorithm impl.,
+    // which store the collision manifold) are actually different, even if the
+    // objects that the pair consists of (or more specifically broadphase
+    // proxies) are the same. As a result, we can't pass the world pair cache
+    // to do the processing because it will only generate contact points for
+    // the pairs of objects in the world. As a result, the contact points (or
+    // manifold) will not be accessible from the ghost pair cache, (whose
+    // contact points have not been calculated).
+    auto ghost_cache = ghost_.getOverlappingPairCache();
+    world->getDispatcher()->dispatchAllCollisionPairs(ghost_cache,
+                                                      world->getDispatchInfo(),
+                                                      world->getDispatcher());
+
     bool penetration = false;
-    world->getDispatcher()->dispatchAllCollisionPairs(
-            ghost_.getOverlappingPairCache(),
-            world->getDispatchInfo(),
-            world->getDispatcher());
 
-    btScalar maxPen = btScalar(0.0);
-#endif
+    xform = ghost_.getWorldTransform();
+    auto cur_pos = xform.getOrigin();
+
+    for (int i = 0; i < ghost_cache->getNumOverlappingPairs(); i++)
+    {
+      btManifoldArray manifolds;
+
+      btBroadphasePair* pair = &ghost_cache->getOverlappingPairArray()[i];
+
+      btCollisionObject* obj0 = static_cast<btCollisionObject*>(
+              pair->m_pProxy0->m_clientObject);
+      btCollisionObject* obj1 = static_cast<btCollisionObject*>(
+              pair->m_pProxy1->m_clientObject);
+
+      if ((obj0 && !obj0->hasContactResponse()) ||
+          (obj1 && !obj1->hasContactResponse()))
+      {
+        continue;
+      }
+
+      if (pair->m_algorithm)
+      {
+        pair->m_algorithm->getAllContactManifolds(manifolds);
+      }
+
+      for (int j = 0; j < manifolds.size(); j++)
+      {
+        btPersistentManifold* manifold = manifolds[j];
+        btScalar directionSign =
+          manifold->getBody0() == &ghost_ ? btScalar(-1.0) : btScalar(1.0);
+
+        for (int p = 0; p < manifold->getNumContacts(); p++)
+        {
+          const btManifoldPoint& pt = manifold->getContactPoint(p);
+
+          btScalar dist = pt.getDistance();
+
+          if (dist < 0.0)
+          {
+            cur_pos += pt.m_normalWorldOnB * directionSign * dist * btScalar(0.2);
+            penetration = true;
+          }
+        }
+      }
+    }
+    xform.setOrigin(cur_pos);
 
     btVector3 local_dpos(0.0f, 0.0f, 0.0f);
 
