19a476a835ce3aa2e080cf1e10c4cad4eb4bdc2a
Luke San Antonio <lukesanantonio@gmail.com>
2016-07-18 01:27:24 -0400

    Spawn the player in the center of the map

--- src/player.cpp
+++ src/player.cpp
@@ -55,7 +55,7 @@ namespace redc
     // Transform
     btTransform trans;
     trans.setIdentity();
-    trans.setOrigin(btVector3(3.0f, 3.0f, 0.0f));
+    trans.setOrigin(btVector3(0.0f, 2.0f, 0.0f));
     ghost_.setWorldTransform(trans);
 
     // Shape
