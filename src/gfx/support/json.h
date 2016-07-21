2a68fda5672af426733fb9bb9c82bf7318cdc9ca
Luke San Antonio <lukesanantonio@gmail.com>
2016-07-21 03:06:03 -0400

    Update / add vec3 => JSON functions
    Add support for array vectors like this [0.2, 12.5, 4.1]

--- src/gfx/support/json.h
+++ src/gfx/support/json.h
@@ -7,8 +7,12 @@
 #include "glm/glm.hpp"
 namespace redc
 {
-  inline glm::vec3 vec3_from_js(rapidjson::Value const& v) noexcept
+  inline glm::vec3 vec3_from_js_object(rapidjson::Value const& v) noexcept
   {
     return glm::vec3{v["x"].GetDouble(),v["y"].GetDouble(),v["z"].GetDouble()};
+  }
+  inline glm::vec3 vec3_from_js_array(rapidjson::Value const& v) noexcept
+  {
+    return glm::vec3{v[0].GetDouble(),v[1].GetDouble(),v[2].GetDouble()};
   }
 }
