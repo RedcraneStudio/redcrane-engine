3495a9ed1da34fb03a6745ac52e22b16fdd4e1fe
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2016-08-26 15:24:01 -0400

    Be specific about OpenGL context initialization
    By specifying alpha, we work around the weird sRGB bug in Intel mesa
    drivers. This code does not work on NVIDIA!! A workaround is needed.

--- src/sdl_helper.cpp
+++ src/sdl_helper.cpp
@@ -74,9 +74,20 @@ namespace redc
 
     // Initialize OpenGL context
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0);
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
     SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
-                        SDL_GL_CONTEXT_PROFILE_CORE);
 
     ret.gl_context = SDL_GL_CreateContext(ret.window);
     if(ret.gl_context == NULL)
