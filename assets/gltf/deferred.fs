68b12786f242c9aa13a35b2c8927d5506e8156b1
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2016-09-17 17:48:19 -0400

    Use clearer _cam suffix in deferred shaders
    Before it was _world, which is just wrong. The coordinates in question
    were all in camera space.

--- assets/gltf/deferred.fs
+++ assets/gltf/deferred.fs
@@ -3,8 +3,8 @@ layout(location = 0) out vec4 pos;
 layout(location = 1) out vec4 normal;
 layout(location = 2) out vec4 color;
 
-in vec3 position_world;
-in vec3 normal_world;
+in vec3 position_cam;
+in vec3 normal_cam;
 
 uniform vec3 diffuse;
 uniform float shininess;
@@ -13,7 +13,7 @@ void main()
 {
   // Record the post-projection-space depth so we can easily write it to the
   // depth buffer later.
-  pos = vec4(position_world, gl_FragCoord.z);
-  normal = vec4(normal_world, gl_FragCoord.w);
+  pos = vec4(position_cam, gl_FragCoord.z);
+  normal = vec4(normal_cam, gl_FragCoord.w);
   color = vec4(diffuse, shininess);
 }
--- assets/gltf/deferred.vs
+++ assets/gltf/deferred.vs
@@ -8,14 +8,14 @@ uniform mat3 normal_mat;
 uniform mat4 modelview;
 uniform mat4 proj;
 
-out vec3 position_world;
-out vec3 normal_world;
+out vec3 position_cam;
+out vec3 normal_cam;
 
 void main()
 {
-  normal_world = normal_mat * normal;
-  position_world = vec3(modelview * vec4(position, 1.0));
+  normal_cam = normal_mat * normal;
+  position_cam = vec3(modelview * vec4(position, 1.0));
 
   // Screen space position
-  gl_Position = proj * vec4(position_world, 1.0);
+  gl_Position = proj * vec4(position_cam, 1.0);
 }
